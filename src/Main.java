2ee397f6bfff68d13cf0eaf055b7d2b69ffe4cf3
BINHNGHIEN <haibinh.94.bui@gmail.com>
2022-03-24 17:08:42 +0700

    noidung

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int size = input.nextInt();
+
+
+        int[] arrNumber = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter the ["+i+"] element:");
+            arrNumber[i] = input.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(arrNumber[i] + "\t");
+        }
+        System.out.println("\n");
+        int min = arrNumber[0];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if(arrNumber[i] < arrNumber[0]) {
+                min = arrNumber[i];
+                index = i;
+            }
+        }
+        System.out.println("The min element in array is: " + min + " at position " + index);
+    }
+}
